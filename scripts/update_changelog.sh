55e4b98862c36447ff2463c3c3073331069cf484
kewe63 <86300262+Kewe63@users.noreply.github.com>
2024-03-03 11:42:53 +0300

    Update update_changelog.sh
    I added explanations to each part of the code and made some updates to make it more readable.  Added a few functions for code repetition.

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-# This is a wrapper around `github_changelog_generator` (https://github.com/github-changelog-generator)
+# This script is a wrapper around `github_changelog_generator` (https://github.com/github-changelog-generator)
 # to simplify / automate updating of the CHANGELOG.md file.
 #
 # Originally developed for CosmWasm cw_plus (https://github.com/CosmWasm/cw-plus) repository.
+
 set -o errexit -o pipefail
 
-ORIGINAL_OPTS=$*
-# Requires getopt from util-linux 2.37.4 (brew install gnu-getopt on Mac)
-OPTS=$(getopt -l "help,release-branch:,since-tag:,future-release:,full,token:" -o "hft" -- "$@") || exit 1
-
+# Prints usage of the script.
 function print_usage() {
-    echo -e "Usage: $0 [-h|--help] [-f|--full] [--release-branch <branch>] [--since-tag <tag>] [--future-release] <tag> [-t|--token <token>]
+    cat <<EOF
+Usage: $0 [-h|--help] [-f|--full] [--release-branch <branch>] [--since-tag <tag>] [--future-release <tag>] [-t|--token <token>]
 
--h, --help                Display help.
--f, --full                Process changes since the beginning (by default: since latest git version tag).
---since-tag <tag>         Process changes since git version tag <tag> (by default: since latest git version tag).
---future-release <tag>    Put the unreleased changes in the specified <tag>.
---release-branch <branch> Limit pull requests to the release branch <branch>.
---token <token>           Pass changelog github token <token>."
+  -h, --help                Display help.
+  -f, --full                Process changes since the beginning (by default: since latest git version tag).
+  --since-tag <tag>         Process changes since git version tag <tag> (by default: since latest git version tag).
+  --future-release <tag>    Put the unreleased changes in the specified <tag>.
+  --release-branch <branch> Limit pull requests to the release branch <branch>.
+  --token <token>           Pass changelog github token <token>.
+EOF
 }
 
-function remove_opt() {
-    ORIGINAL_OPTS=$(echo "$ORIGINAL_OPTS" | sed "s/\\B$1\\b//")
-}
+# Parameters
+ORIGINAL_OPTS=$*
+OPTS=$(getopt -l "help,release-branch:,since-tag:,future-release:,full,token:" -o "hft" -- "$@") || exit 1
 
 eval set -- "$OPTS"
 while true
@@ -55,6 +55,7 @@ echo "Github user: $GITHUB_USER"
 GITHUB_REPO=$(echo $ORIGIN_URL | sed -n 's#.*/\([^.]*\).*#\1#p')
 echo "Github repo: $GITHUB_REPO"
 
+# Check tag
 if [ -z "$TAG" ]
 then
   # Use latest git version tag
